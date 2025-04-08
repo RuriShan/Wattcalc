ecfdf90676c0c293cb6d85e0aff14c7db39a5785
Pedro Gnatta Sureck <130069914+RuriShan@users.noreply.github.com>
2025-04-08 18:29:57 +0000

    Finalização calculadora...

--- app.py
+++ app.py
@@ -1,8 +1,53 @@
 from flask import Flask, render_template, request, url_for, redirect
-from calc import watt
 app = Flask(__name__)
+
+def watt(Tensão, Corrente):
+    V = Tensão
+    A = Corrente
+
+    Watt = V * A
+    return Watt
+
+def ampere(Tensão, Potencia):
+    Ampere = Tensão / Potencia
+    return Ampere
+
+def volt(Corrente, Ohm):
+    Volt = Corrente * Ohm
+    return Volt
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    a = watt()
-    return render_template('main.html', a=a)
+    if request.method =='POST':
+        try:
+            Potencia = float(request.form['potencia'])
+        except:
+            Potencia = 0
+
+        try:
+            Tensão = float(request.form['tensao'])
+        except:
+            Tensão = 0
+        
+        try:
+            Corrente = float(request.form['corrente'])
+        except:
+            Corrente = 0
+        
+        try:
+            Ohm = float(request.form['Ohm'])
+        except:
+            Ohm = 0
+
+        a = watt(Tensão, Corrente)
+        try:
+            b = ampere(Tensão, Potencia)
+        except:
+            b = 0
+        c = volt(Corrente, Ohm)
+        return render_template('main.html', a=a, b=b, c=c)
+    else:
+        a = 0
+        b = 0
+        c = 0
+        return render_template('main.html', a=a, b=b, c=c)
